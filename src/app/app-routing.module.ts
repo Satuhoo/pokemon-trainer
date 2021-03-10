3e38b3c3c9ccf84743fddafc515be29c178b7a37
satu.heikkonen <satu.heikkonen@5CG05206R0.nordic.local>
2021-03-10 13:49:29 +0200

    feat: adds router module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { TrainerPage } from './pages/trainer-page/trainer-page.component';
+import { LoginPage } from './pages/login-page/login-page.component';
+import { PokemonCataloguePage } from './pages/pokemon-catalogue-page/pokemon-catalogue-page.component';
+import { PokemonDetailPage } from './pages/pokemon-detail-page/pokemon-detail-page.component';
+
+const routes :Routes = [{
+    path: 'login',
+    component: LoginPage
+},{
+    path: '',
+    pathMatch: 'full',
+    component: TrainerPage
+},{
+    path: 'catalogue',
+    component: PokemonCataloguePage
+},{
+    path: 'pokemon/:name',
+    component: PokemonDetailPage
+}]
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule{}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { AppHeaderComponent } from './components/shared/app-header/app-header.component';
@@ -20,7 +21,8 @@ import { TrainerPage } from './pages/trainer-page/trainer-page.component';
     TrainerPage,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
