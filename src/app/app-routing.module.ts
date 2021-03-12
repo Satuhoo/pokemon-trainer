ec9874322bb724531846ae076a0428626f8c3a1e
satu.heikkonen <satu.heikkonen@5CG05206R0.nordic.local>
2021-03-12 17:05:33 +0200

    fix: corrects paths in route module and adds error page if path doesn't match

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,23 +5,30 @@ import { LoginPage } from './pages/login-page/login-page.component';
 import { PokemonCataloguePage } from './pages/pokemon-catalogue-page/pokemon-catalogue-page.component';
 import { PokemonDetailPage } from './pages/pokemon-detail-page/pokemon-detail-page.component';
 import { SessionGuard } from './guards/session.guard';
+import { NotFoundPage } from './pages/not-found-page/not-found-page.component';
 
 const routes :Routes = [{
-    path: 'login',
-    component: LoginPage
-},{
     path: '',
     pathMatch: 'full',
-    component: TrainerPage,
-    canActivate: [SessionGuard]
+    redirectTo: '/pokemons'
+},{
+    path: 'login',
+    component: LoginPage
 },{
     path: 'pokemons',
     component: PokemonCataloguePage,
     canActivate: [SessionGuard]
 },{
+    path: 'trainer',
+    component: TrainerPage,
+    canActivate: [SessionGuard]
+},{
     path: 'pokemons/:id',
     component: PokemonDetailPage,
     canActivate: [SessionGuard]
+},{
+    path: '**',
+    component: NotFoundPage
 }]
 
 @NgModule({
